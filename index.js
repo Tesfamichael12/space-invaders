d408384b0df86d7d1a96ba389f7bb15926dcb544
Tesfamichael12 <tesfamichael132@gmail.com>
2024-12-08 05:19:52 +0300

    fix the width of the canvas / aspect ratio of 16:9.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const score = document.querySelector('#scoreEl')
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth
-canvas.height = innerHeight
+canvas.width = 1024
+canvas.height = 576
 
 // Player
 class Player {
