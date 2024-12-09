da530f5af71791747318652c5e7c93d1291a19f8
Tesfamichael12 <tesfamichael132@gmail.com>
2024-12-10 01:02:31 +0300

    updated the path for the audio files.

--- audio.js
+++ audio.js
@@ -1,33 +1,33 @@
 Howler.volume(0.5)
 const audio = {
   backgroundMusic: new Howl({
-    src: './public/audio/backgroundMusic.wav',
+    src: './audio/backgroundMusic.wav',
     loop: true
   }),
   bomb: new Howl({
-    src: './public/audio/bomb.mp3'
+    src: './audio/bomb.mp3'
   }),
   bonus: new Howl({
-    src: './public/audio/bonus.mp3',
+    src: './audio/bonus.mp3',
     volume: 0.8
   }),
   enemyShoot: new Howl({
-    src: './public/audio/enemyShoot.wav'
+    src: './audio/enemyShoot.wav'
   }),
   explode: new Howl({
-    src: './public/audio/explode.wav'
+    src: './audio/explode.wav'
   }),
   gameOver: new Howl({
-    src: './public/audio/gameOver.mp3'
+    src: './audio/gameOver.mp3'
   }),
   select: new Howl({
-    src: './public/audio/select.mp3'
+    src: './audio/select.mp3'
   }),
   shoot: new Howl({
-    src: './public/audio/shoot.wav'
+    src: './audio/shoot.wav'
   }),
   start: new Howl({
-    src: './public/audio/start.mp3'
+    src: './audio/start.mp3'
   })
 }
 
